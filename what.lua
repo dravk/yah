4f937318c61a2db716c4aae80742a93a0dc79489
dravk <97814009+dravk@users.noreply.github.com>
2022-05-21 03:21:03 +0300

    Update what.lua

--- what.lua
+++ what.lua
@@ -1,1 +1,7 @@
+local msgbox = messagebox("allah var mi???", "Sorgu Melegi 31",4)
 
+if msgbox == 6 then
+    local msgbox2 = messagebox("afferim", "Sorgu Melegi 31",0)
+elseif msgbox == 7 then
+    local msgbox3 = messagebox("senin gibi kulun anasini sikim orospu cocu", "Azrail", 0)
+end
