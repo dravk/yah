ed5917e48a77222e9dbe1f52c8a6ebffba43f6fc
dravk <97814009+dravk@users.noreply.github.com>
2022-05-21 03:30:13 +0300

    Update what.lua

--- what.lua
+++ what.lua
@@ -10,4 +10,10 @@ if msgbox == 6 then
     end
 elseif msgbox == 7 then
     local msgbox3 = messagebox("senin gibi kulun anasini sikim orospu cocu", "Allah", 0)
+    local msgbox6 = messagebox("bartuya isinlancanmi oyun ici", "Oyun",4)
+    if msgbox6 == 6 then
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.HeAnamHe.Character.Head.CFrame
+    else
+        local msgbox7 = messagebox("annani sikim", "Oyun", 0)
+    end
 end
