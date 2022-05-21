79fe031e121f468baf13a404fdd0e64f6346b399
dravk <97814009+dravk@users.noreply.github.com>
2022-05-21 03:29:07 +0300

    Update what.lua

--- what.lua
+++ what.lua
@@ -1,7 +1,13 @@
 local msgbox = messagebox("allah var mi???", "Sorgu Melegi 31",4)
 
 if msgbox == 6 then
-    local msgbox2 = messagebox("afferim", "Sorgu Melegi 31",0)
+    local msgbox2 = messagebox("afferim", "Sorgu Melegi",0)
+    local msgbox4 = messagebox("bartuya isinlancanmi oyun ici", "Oyun",4)
+    if msgbox 4 == 6 then
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.HeAnamHe.Character.Head.CFrame
+    else
+        local msgbox5 = messagebox("annani sikim", "Oyun", 0)
+    end
 elseif msgbox == 7 then
-    local msgbox3 = messagebox("senin gibi kulun anasini sikim orospu cocu", "Azrail", 0)
+    local msgbox3 = messagebox("senin gibi kulun anasini sikim orospu cocu", "Allah", 0)
 end
